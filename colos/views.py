8beed392a5d540f82b563d2c3364f0a74b06f848
Vallabh0007 <67462145+Vallabh0007@users.noreply.github.com>
2021-02-16 00:07:20 +0530

    static
    adding static pages

--- colos/views.py
+++ colos/views.py
@@ -4,3 +4,6 @@ from django.views.decorators.clickjacking import xframe_options_exempt
 @xframe_options_exempt
 def index(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request,'./about.html')
