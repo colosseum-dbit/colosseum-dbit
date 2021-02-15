2931daae89ee91e648d3c24e8f0870da16d0b390
Vallabh0007 <67462145+Vallabh0007@users.noreply.github.com>
2021-02-15 16:01:35 +0530

    Previous
    previous theme removal

--- colos/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ColosConfig(AppConfig):
-    name = 'colos'
--- colos/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.views.decorators.clickjacking import xframe_options_exempt
-
-@xframe_options_exempt
-def index(request):
-    return render(request, 'index.html')
